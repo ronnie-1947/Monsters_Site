a3d44a054d890a86ec827effb3c72338ad7ea471
Ronny <ripunjoy.buddha@gmail.com>
2020-07-12 19:34:42 +0530

    Updates

---
+++ precache-manifest.c40c40f64f962cfffc3fdc2191bd2275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb52376a2578d41139b2471676e0f538",
+    "url": "/Monsters_Site/index.html"
+  },
+  {
+    "revision": "13b35e78c81da9904548",
+    "url": "/Monsters_Site/static/css/main.a964d5b7.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Monsters_Site/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters_Site/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13b35e78c81da9904548",
+    "url": "/Monsters_Site/static/js/main.7f694b04.chunk.js"
+  },
+  {
+    "revision": "1b7fc1d0a9c75cdac4cd",
+    "url": "/Monsters_Site/static/js/runtime-main.11e31324.js"
+  }
+]);
